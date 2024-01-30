2cc2e4430db9d85b177fe2330a2e01685580c2f7
Almudena Rendón Fernández <126793941+Almudena-Rendon@users.noreply.github.com>
2024-01-30 21:55:02 +0100

    style(hero-section): fix padding in h1 in  responsive designs

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,12 +13,12 @@ const Hero = () => {
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-36">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-36">
         <p className="text-xl font-montserrat text-purple-900">
           Our Summer Collection
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[70px] max-sm:leading-[82px] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[68px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 xl:pr-10 max-sm::p-0">
             New Collection
           </span>
           <br />
